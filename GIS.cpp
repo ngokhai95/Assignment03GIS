bb55e4592a3d6b268438fe7e78ce573b64851d6e
Khai <ngokhai1995@gmail.com>
2020-04-04 20:24:11 -0700

    First Commit

---
+++ GIS.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+void parser(string scriptFile);
+void import(string databaseFile);
+void debug(string databaseFile);
+char cmdStat;
+
+void main()
+{
+	parser("DemoScript01.txt");
+}
+
+void parser(string scriptFile)
+{
+	std::ifstream file(scriptFile);
+	std::string line;
+	
+	while (std::getline(file, line))
+	{
+		vector<string> words;
+		if (line[0] != ';')
+		{
+			string word;		
+			istringstream ss(line);
+			do
+			{
+				ss >> word;
+				if (word == "world")
+				{
+
+				}
+				else if (word == "import")
+				{
+
+				}
+				else if (word == "debug")
+				{
+
+				}
+				else if (word == "what_is_at")
+				{
+
+				}
+				else if (word == "what_is")
+				{
+
+				}
+				else if (word == "what_is_in")
+				{
+
+				}
+				else if (word == "quit")
+				{
+
+				}
+			} 
+			while (ss);
+		}
+		for (auto word : words)
+		{
+			cout << word << endl;
+		}
+	}
+
+	
+}
+
+void import(string databaseFile)
+{
+
+}
+
+void debug(string databaseFile)
+{
+
+}
