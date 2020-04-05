e95f845eb22ef9511e4044cba39aa89ece94e2e5
Khai <ngokhai1995@gmail.com>
2020-04-04 22:49:13 -0700

    Update 1

--- GIS.cpp
+++ GIS.cpp
@@ -3,30 +3,236 @@
 #include <math.h>
 #include <string>
 #include <fstream>
-#include <vector>
+#include <queue>
 #include <sstream>
 
 using namespace std;
 
-void parser(string scriptFile);
-void import(string databaseFile);
-void debug(string databaseFile);
-char cmdStat;
+int cmdStat = 0;
 
-int main()
+void DMStoString(string dms)
 {
-	parser("DemoScript01.txt");
-	return 0;
+	string degree, minute, second;
+	char direction = ' ';
+	if (dms.size() == 7)
+	{
+		degree = dms.substr(0, 2);
+		minute = dms.substr(2, 2);
+		second = dms.substr(4, 2);
+		direction = dms[6];
+	}
+	else if (dms.size() == 8)
+	{
+		degree = dms.substr(0, 3);
+		minute = dms.substr(3, 2);
+		second = dms.substr(5, 2);
+		direction = dms[7];
+	}
+	cout << degree << " Degree " << minute << " Minute " << second << " Second " << "at " << direction << endl;
+}
+
+int DMStoSecond(string dms)
+{
+	int seconds = 0;
+	int degree, minute, second;
+	char direction;
+	if (dms.size() == 7)
+	{
+		degree = stoi(dms.substr(0, 2));
+		minute = stoi(dms.substr(2, 2));
+		second = stoi(dms.substr(4, 2));
+		direction = dms[6];
+		if (direction == 'W' || direction == 'S')
+		{
+			seconds = -(degree * 3600 + minute * 60 + second);
+		}
+		else if (direction == 'E' || direction == 'N')
+		{
+			seconds = degree * 3600 + minute * 60 + second;
+		}
+	}
+	else if (dms.size() == 8)
+	{
+		degree = stoi(dms.substr(0, 3));
+		minute = stoi(dms.substr(3, 2));
+		second = stoi(dms.substr(5, 2));
+		direction = dms[7];
+		if (direction == 'W' || direction == 'S')
+		{
+			seconds = -(degree * 3600 + minute * 60 + second);
+		}
+		else if (direction == 'E' || direction == 'N')
+		{
+			seconds = degree * 3600 + minute * 60 + second;
+		}
+	}
+	cout << "Seconds: " << seconds << endl;
+	return seconds;
+}
+
+template <typename T>
+void world(string databaseFile, queue<T>& data)
+{
+	while (!data.empty()) 
+	{
+		if (data.size() == 1)
+		{
+			data.pop();
+		}
+		else
+		{
+			DMStoString(data.front());
+			DMStoSecond(data.front());
+			data.pop();
+		}
+	}
+	cout << endl;
+}
+
+template <typename T>
+void import(string databaseFile, queue<T>& data)
+{
+	while (!data.empty())
+	{
+		if (data.size() == 1)
+		{
+			data.pop();
+		}
+		else
+		{
+			cout << data.front() << " ";
+			data.pop();
+		}
+	}
+	cout << endl;
+}
+
+template <typename T>
+void debug(string databaseFile, queue<T>& data)
+{
+	while (!data.empty())
+	{
+		if (data.size() == 1)
+		{
+			data.pop();
+		}
+		else
+		{
+			cout << data.front() << " ";
+			data.pop();
+		}
+	}
+	cout << endl;
+}
+
+template <typename T>
+void whatisat(string databaseFile, queue<T>& data)
+{
+	while (!data.empty())
+	{
+		if (data.size() == 1)
+		{
+			data.pop();
+		}
+		else
+		{
+			cout << data.front() << " ";
+			data.pop();
+		}
+	}
+	cout << endl;
+}
+
+template <typename T>
+void whatis(string databaseFile, queue<T>& data)
+{
+	while (!data.empty())
+	{
+		if (data.size() == 1)
+		{
+			data.pop();
+		}
+		else
+		{
+			cout << data.front() << " ";
+			data.pop();
+		}
+	}
+	cout << endl;
+}
+
+template <typename T>
+void whatisin(string databaseFile, queue<T>& data)
+{
+	while (!data.empty())
+	{
+		if (data.size() == 1)
+		{
+			data.pop();
+		}
+		else
+		{
+			cout << data.front() << " ";
+			data.pop();
+		}
+	}
+	cout << endl;
+}
+
+template <typename T>
+void execute(int cmd, queue<T>& data)
+{
+	if (cmd == 0)
+	{
+		return;
+	}
+
+	switch (cmd)
+	{
+		case 1:
+			cout << "Execute CMD: World " << endl << "World boundaries are set to: " << endl;
+			world("yes", data);
+			cmdStat = 0;
+			break;
+		case 2:
+			cout << "Execute CMD: Import File Name: ";
+			import("yes", data);
+			cout << "Importing..." << endl;
+			cout << "Import Success!" << endl;
+			cmdStat = 0;
+			break;
+		case 3:
+			cout << "Execute CMD: Debug ";
+			debug("yes", data);
+			cmdStat = 0;
+			break;
+		case 4:
+			cout << "Execute CMD: What_is_at ";
+			whatisat("yes", data);
+			cmdStat = 0;
+			break;
+		case 5:
+			cout << "Execute CMD: What_is ";
+			whatis("yes", data);
+			cmdStat = 0;
+			break;
+		case 6:
+			cout << "Execute CMD: What_is_in ";
+			whatisin("yes", data);
+			cmdStat = 0;
+			break;
+	}
 }
 
 void parser(string scriptFile)
 {
 	std::ifstream file(scriptFile);
 	std::string line;
-	
+	queue<string> data;
+
 	while (std::getline(file, line))
 	{
-		vector<string> words;
+		
 		if (line[0] != ';')
 		{
 			string word;		
@@ -36,50 +242,55 @@ void parser(string scriptFile)
 				ss >> word;
 				if (word == "world")
 				{
-
+					execute(cmdStat, data);
+					cmdStat = 1;
 				}
 				else if (word == "import")
 				{
-
+					execute(cmdStat, data);
+					cmdStat = 2;
 				}
 				else if (word == "debug")
 				{
-
+					execute(cmdStat, data);
+					cmdStat = 3;
 				}
 				else if (word == "what_is_at")
 				{
-
+					execute(cmdStat, data);
+					cmdStat = 4;
 				}
 				else if (word == "what_is")
 				{
-
+					execute(cmdStat, data);
+					cmdStat = 5;
 				}
 				else if (word == "what_is_in")
 				{
-
+					execute(cmdStat, data);
+					cmdStat = 6;
 				}
 				else if (word == "quit")
 				{
-
+					execute(cmdStat, data);
+					cout << "Finish parsing script" << endl;
+					system("pause");
+				}
+				else
+				{
+					data.push(word);
 				}
 			} 
 			while (ss);
-		}
-		for (auto word : words)
-		{
-			cout << word << endl;
 		}
 	}
 
 	
 }
 
-void import(string databaseFile)
+
+int main()
 {
-
-}
-
-void debug(string databaseFile)
-{
-
+	parser("DemoScript03.txt");
+	return 0;
 }
